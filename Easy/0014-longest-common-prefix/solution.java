1069999871717fd6531aa1a37bb8d9d7c0fed83f
김기영 <cla12093@gmail.com>
2026-09-16 11:11:08 +0900

    solve: [Easy] 14. Longest Common Prefix (java)

--- Easy/0014-longest-common-prefix/solution.java
+++ Easy/0014-longest-common-prefix/solution.java
@@ -7,11 +7,11 @@ class Solution {
         String last = strs[strs.length - 1];
 
         for(int i = 0; i < Math.min(first.length(), last.length()); i++) {
-            if(first.charAt(i) == last.charAt(i)) {
-                sb.append(first.charAt(i));
-            } else { 
+            if(first.charAt(i) != last.charAt(i)) {
                 return sb.toString();
+                
             }
+            sb.append(first.charAt(i));
         }
 
         return sb.toString();
