9c7b2d0d39d3a5fa9917655fef085dfd139af8b7
김기영 <cla12093@gmail.com>
2026-09-17 12:15:56 +0900

    solve: [Easy] 242. Valid Anagram (java)

--- Easy/0242-valid-anagram/solution.java
+++ Easy/0242-valid-anagram/solution.java
@@ -1,37 +1,10 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        Map<Character, Integer> strMap = new HashMap<>();
-
         if(s.length() != t.length()) return false;
-
-        for(int i = 0; i < s.length(); i++) {
-            char sLetter = s.charAt(i);
-            char tLetter = t.charAt(i);
-
-            if(strMap.containsKey(sLetter)) {
-                if(strMap.get(sLetter) == -1) {
-                    strMap.remove(sLetter);
-                } else {
-                    strMap.put(sLetter, strMap.get(sLetter) + 1);
-                }
-            } else {
-                strMap.put(sLetter, 1);
-            }
-
-            if(strMap.containsKey(tLetter)) {
-                if(strMap.get(tLetter) == 1) {
-                    strMap.remove(tLetter);
-                } else {
-                    strMap.put(tLetter, strMap.get(tLetter) - 1);
-                }
-            } else {
-                strMap.put(tLetter, -1);
-            }
-        }
-
-        if(strMap.size() == 0) {
-            return true;
-        }
-        return false;
+        char[] sArray = s.toCharArray();
+        char[] tArray = t.toCharArray();
+        Arrays.sort(sArray);
+        Arrays.sort(tArray);
+        return Arrays.equals(sArray, tArray);
     }
 }
